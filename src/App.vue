a27d88d87515bcd33ef95301ded7c83c1aeb72e6
JOBOYA <lena.boya26@gmail.com>
2023-10-03 15:07:48 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="question" :style="{ opacity: opacity }">
+      <span 
+        v-for="(letter, index) in displayedQuestion.split('')" 
+        :key="index" 
+        class="fixed-width"
+      >
+        {{ letter }}
+      </span>
+    </div>
+    <input v-model="input" @keyup.enter="nextQuestion" />
+  </div>
 </template>
 
+
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import questions from './data/questions.js'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      questions: questions,
+      currentQuestionIndex: 0,
+      input: '',
+      currentQuestion: '',
+      displayedQuestion: '',
+      opacity: 1
+    }
+  },
+  mounted() {
+    this.currentQuestion = this.questions[this.currentQuestionIndex].text;
+    this.displayedQuestion = this.currentQuestion;
+  },
+  methods: {
+    async fadeOut() {
+      this.opacity = 0;
+      await new Promise(resolve => setTimeout(resolve, 500)); // Pause de 500ms pour l'effet d'opacité
+    },
+    async scrambleText() {
+      const originalText = this.currentQuestion;
+      let iterations = 10;
+      
+      while (iterations > 0) {
+        const scrambled = originalText.split('').map(char => {
+          if (char === ' ') return ' ';
+          const randomChar = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+          return randomChar;
+        }).join('');
+        
+        this.displayedQuestion = scrambled;
+        
+        await new Promise(resolve => setTimeout(resolve, 100));
+        iterations--;
+      }
+      
+      await this.fadeOut();
+      
+      this.displayedQuestion = originalText;
+    },
+    async nextQuestion() {
+      await this.scrambleText();
+      
+      if (this.currentQuestionIndex < this.questions.length - 1) {
+        this.currentQuestionIndex++;
+        this.currentQuestion = this.questions[this.currentQuestionIndex].text;
+        this.displayedQuestion = this.currentQuestion;
+      } else {
+        // Faire quelque chose lorsque toutes les questions sont terminées
+      }
+      
+      this.input = '';
+      this.opacity = 1;  // Réinitialiser l'opacité pour la prochaine question
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#question {
+  transition: opacity 0.5s ease-in-out;
+  font-family: "Courier New", monospace; /* Police à chasse fixe */
+}
+
+.fixed-width {
+  display: inline-block;
+  width: 1ch;  /* Largeur fixe pour chaque caractère */
 }
 </style>
---
+++ src/data/questions.js
@@ -0,0 +1,47 @@
+export default [
+    {
+      id: 1,
+      text: 'Ressentez-vous des étourdissements lorsque vous vous levez ?',
+    },
+    {
+      id: 2,
+      text: 'Avez-vous des épisodes de vertiges en position assise ?',
+    },
+    {
+      id: 3,
+      text: 'Les vertiges sont-ils accompagnés de nausées ?',
+    },
+    {
+      id: 4,
+      text: 'Avez-vous des problèmes d’équilibre récemment ?',
+    },
+    {
+      id: 5,
+      text: 'Avez-vous des problèmes d’audition ?',
+    },
+    {
+      id: 6,
+      text: 'Avez-vous des problèmes de vision ?',
+    },
+    {
+      id: 7,
+      text: 'Avez-vous des problèmes de mémoire ?',
+    },
+    {
+      id: 8,
+      text: 'Avez-vous des problèmes de concentration ?',
+    },
+    {
+      id: 9,
+      text: 'Avez-vous des problèmes de sommeil ?',
+    },
+    {
+      id: 10,
+      text: 'Avez-vous des problèmes de déglutition ?',
+    },
+    {
+      id: 11,
+      text: 'Avez-vous des problèmes de parole ?',
+    }
+  ];
+  
